7d8ba34abe33727fa2140ad96e507fc4a40a7572
asapkent <jeffersalex94@gmail.com>
2020-10-31 23:57:16 -0400

    set up nav controller.

---
+++ PickAColor/ColorTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTableViewController.swift
+//  PickAColor
+//
+//  Created by Robert Jeffers on 10/31/20.
+//
+
+import UIKit
+
+class ColorTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ PickAColor/SelectedColorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectedColorViewController.swift
+//  PickAColor
+//
+//  Created by Robert Jeffers on 10/31/20.
+//
+
+import UIKit
+
+class SelectedColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
