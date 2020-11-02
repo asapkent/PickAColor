6584a67e7c1cb118e6eca0fd30eced311be26066
asapkent <jeffersalex94@gmail.com>
2020-11-02 17:07:07 -0500

    making sure data shows in table view  rowss

--- PickAColor/ColorTableViewController.swift
+++ PickAColor/ColorTableViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class ColorTableViewController: UIViewController {
+var numbers = ["5","6","7","8","9","10","11","12","13","44","90"]
 
+class ColorTableViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+}
+
+extension ColorTableViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let number = numbers[indexPath.row]
+        
+        cell.textLabel?.text = number
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "colorvc", sender: nil)
+    }
 }
